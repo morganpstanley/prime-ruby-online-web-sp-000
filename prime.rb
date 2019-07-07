4fa5ed854f58533e5f8852a56c7241d2035f20d6
Morgan Stanley <morganpstanley@gmail.com>
2019-07-07 03:15:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require 'pry'
+
+def prime?(arg)
+  is_prime = Array(2..arg)
+  if arg == 2
+    true
+  elsif arg < 0 || arg == 1
+    false
+  else
+    is_prime.one? do |test|
+      arg % test == 0
+    end
+  end
+end
